dd4912128278b2cd3bec3285d9ff5432ca821fb7
kuba <kuba5k5@gmail.com>
2019-12-05 11:46:57 +0100

    dodanie operacji na bazie danych

--- create.js
+++ create.js
@@ -1,43 +1,25 @@
 import uuid from "uuid";
-import AWS from "aws-sdk";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
 
-//AWS.config.update({ region: "us-east-2"});
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now()
+    }
+  };
 
-export function main(event, context, callback) {
-    const data = JSON.parse(event.body);
-
-    const params = {
-        TableName: proccess.env.tableName,
-        Item: {
-            userId: event.requestContext.identity.cognitoIdentityId,
-            noteId: uuid.v1(),
-            content: data.content,
-            tags: data.tags,
-            attachment: data.attachment,
-            createdAt: Date.now()
-        }
-    };
-    dynamoDb.put(params, (error, data) => {
-        const headers  = { 
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true
-        };
-        
-        if (error){
-            const response = {
-                statusCode: 500,
-                headers: headers,
-                body: JSON.stringify({ status: false })
-            };
-            callback(null, response);
-            return;
-        }
-        const response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(params.Item)
-        };
-        callback(null, response);
-    });
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
 }
